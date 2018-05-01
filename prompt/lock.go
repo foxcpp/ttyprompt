bec9979dbc5d8970627976c8ea6c33068ec5feac
fox.cpp <fox.cpp@disroot.org>
2018-05-01 15:31:14 +0000

    Add user-friendly message when waiting on TTY lock

--- prompt/lock.go
+++ prompt/lock.go
@@ -1,6 +1,7 @@
 package prompt
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"syscall"
@@ -16,6 +17,13 @@ func UnlockTTY(tty *os.File) {
 
 func LockTTY(tty *os.File) {
 	log.Println("Locking", tty.Name()+"...")
+	err := syscall.FcntlFlock(tty.Fd(), syscall.F_SETLK, &syscall.Flock_t{
+		Type: syscall.F_WRLCK,
+		Pid:  int32(os.Getpid()),
+	})
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Another ttyprompt instance is using requested TTY prompt. Waiting for it to finish...")
+	}
 	syscall.FcntlFlock(tty.Fd(), syscall.F_SETLKW, &syscall.Flock_t{
 		Type: syscall.F_WRLCK,
 		Pid:  int32(os.Getpid()),
